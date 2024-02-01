7ce64e7cbcd3bba89094e2da54a3a928b812267a
zhuiluoi <gaochengduo360@gmail.com>
2024-02-01 21:33:59 +0900

    Upload exercise code
    Upload exercise code

--- test_01_31_1/src/Test.java
+++ test_01_31_1/src/Test.java
@@ -1,5 +1,12 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("/\"hello/\"");
+        short a =128;
+        byte b =(byte) a;
+        System.out.println(a);
+        System.out.println(b);
+
+
     }
 }
