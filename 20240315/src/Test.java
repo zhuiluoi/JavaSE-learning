4904509e43fd3110eb76e328ed3539ef4ad40acf
zhuiluoi <gaochengduo360@gmail.com>
2024-03-16 05:39:38 +0900

    Upload JavaSE practice code
    Upload JavaSE practice code

---
+++ 20240315/src/Test.java
@@ -0,0 +1,24 @@
+public class Test {
+
+    /*public void getCustomerInfo() {
+
+        try {
+
+            // do something that may cause an Exception
+
+        } catch (java.io.FileNotFoundException ex) {
+
+            System.out.print("FileNotFoundException!");
+
+        } catch (java.io.IOException ex) {
+
+            System.out.print("IOException!");
+
+        } catch (java.lang.Exception ex) {
+
+            System.out.print("Exception!");
+
+        }
+
+    }*/
+}
